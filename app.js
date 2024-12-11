720d163eb7c0a0baafc34cdb241dcba83f2df307
unai <unaicc2003@gmail.com>
2024-12-11 12:31:12 +0100

    connect db success

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from 'cors'; // prevent CORS errors
+import connectDB from './database/db.js'; // connect to database
 const port = 444;
 const app = express();
 app.use(cors());
 app.use(express.json()); // parse JSON bodies
+connectDB(); // connect to database
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
--- database/db.js
+++ database/db.js
@@ -17,10 +17,7 @@ using moongose
 import mongoose from "mongoose";
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        useCreateIndex: true,
+        const conn = await mongoose.connect('mongodb+srv://root:toor@teachershub.ge8be.mongodb.net/', {
         });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
